08601463476261a41d7639af7b68023c84b9b357
Dobronszki János <janosdobronszki@citromail.hu>
2012-06-27 16:52:21 +0200

    - Error return instead of nil. - Modified regexp to allow "-" in filename.

--- require.go
+++ require.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
+	"fmt"
 )
 
 const (
@@ -47,8 +48,8 @@ func splitPos(str string, p [][]int) []rep {
 
 // Inserts the files content found in the {{require }} tag into the string.
 // If a file is empty, {{require }} will be replaced with an empty string.
-func Interpret(root, s string, getFile func(string, string) ([]byte, error)) (string, string) {
-	reg, _ := regexp.Compile(beg + "([a-zA-Z.:/])*" + end)
+func Interpret(root, s string, getFile func(string, string) ([]byte, error)) (string, error) {
+	reg, _ := regexp.Compile(beg + "([a-zA-Z.:/-])*" + end)
 	pos := reg.FindAllIndex([]byte(s), -1)
 	r := splitPos(s, pos)
 	fin := ""
@@ -64,16 +65,16 @@ func Interpret(root, s string, getFile func(string, string) ([]byte, error)) (st
 			fin += i.val
 		}
 	}
-	return fin, ""
+	return fin, nil
 }
 
 // R loads a file and Interprets the requires in it.
 // It's a higher order function, we canp rovide our very own getFile func(string) ([]byte,error) method to it so the whole package is more reusable, for example we can implement our own
 // file caching and stuff...
-func R(root, filen string, getFile func(string, string) ([]byte, error)) (string, string) {
+func R(root, filen string, getFile func(string, string) ([]byte, error)) (string, error) {
 	f, err := getFile(root, filen)
 	if err != nil {
-		return "", "file_can_not_be_found"
+		return "", fmt.Errorf("file_can_not_be_found")
 	}
 	fstr := string(f)
 	return Interpret(root, fstr, getFile)
@@ -84,6 +85,6 @@ func gFile(abs, fil string) ([]byte, error) {
 }
 
 // Rsimple is a simplified version of R, it uses ioutil.ReadFile to open files.
-func RSimple(root, filen string) (string, string) {
+func RSimple(root, filen string) (string, error) {
 	return R(root, filen, gFile)
 }
